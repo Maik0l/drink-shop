7d24d35d91d51e6325336bef381f257b922326f4
Maikol Moraes <maikolmoraesas@gmail.com>
2021-08-12 19:17:35 -0300

    feat: media mobile

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,7 +2,8 @@ import { useContext, useEffect } from "react";
 import Card from "../../components/Card";
 import { ProductListContext } from "../../providers/ProductList";
 import api from "../../services/api";
-import { Container } from "./styles";
+import { Container, Content } from "./styles";
+import logo from "../../assets/conifer-whiskey.png";
 
 const Home = () => {
   const { productList, setProductList } = useContext(ProductListContext);
@@ -17,21 +18,30 @@ const Home = () => {
 
   return (
     <Container>
-      {productList.map((item) => (
-        <Card
-          key={item.id}
-          name={item.name}
-          image_url={item.image_url}
-          first_brewed={item.first_brewed}
-          description={item.description}
-          unit={item.volume.unit}
-          value={item.volume.value}
-          item={item}
-          item_id={item.id}
-          type="add"
-          page="home"
-        />
-      ))}
+      <span className="hidden">
+        <img src={logo} alt={"Logo"} />
+        <h1>
+          Welcome to KenzieDrinks! The most famous drink shop around the
+          internet!
+        </h1>
+      </span>
+      <Content>
+        {productList.map((item) => (
+          <Card
+            key={item.id}
+            name={item.name}
+            image_url={item.image_url}
+            first_brewed={item.first_brewed}
+            description={item.description}
+            unit={item.volume.unit}
+            value={item.volume.value}
+            item={item}
+            item_id={item.id}
+            type="add"
+            page="home"
+          />
+        ))}
+      </Content>
     </Container>
   );
 };
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,6 +1,25 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+  span {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 1rem;
+  }
+
+  @media (max-width: 768px) {
+    .hidden {
+      display: none;
+    }
+  }
+
+  img {
+    width: 5rem;
+  }
+`;
+
+export const Content = styled.div`
   display: flex;
   flex-wrap: wrap;
   justify-content: center;
